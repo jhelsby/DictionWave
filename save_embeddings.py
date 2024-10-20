697a1058c4586b8e8f470e7411a01fd09201b1f8
jhels <11036537+jhels@users.noreply.github.com>
2024-10-21 00:54:48 +0100

    Easier embeddings_lite file creation

--- save_embeddings.py
+++ save_embeddings.py
@@ -1,17 +1,22 @@
 from similarity_core import load_embeddings
 import pickle
 
+is_embeddings_lite = False
+
 def main():
     # None loads all words.
     # embeddings_lite has 74000 words - just under 100MB.
-    num_words_to_load = None
+
+    num_words_to_load = 74000 if is_embeddings_lite else None
+
+    output_filename = 'embeddings_lite.pkl' if is_embeddings_lite else 'embeddings.pkl'
 
     word_list, word_vectors, lowercase_word_to_index, lowercase_word_to_word = load_embeddings(num_words_to_load)
 
-    with open('embeddings.pkl', 'wb') as f:
+    with open(output_filename, 'wb') as f:
         pickle.dump((word_list, word_vectors, lowercase_word_to_index, lowercase_word_to_word), f)
 
-    print(f"Loaded {len(word_list)} words from embeddings and saved to embeddings.pkl.")
+    print(f"Loaded {len(word_list)} words from embeddings and saved to {output_filename}.")
 
 if __name__ == "__main__":
     main()
